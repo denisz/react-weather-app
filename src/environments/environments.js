3a81e0fe8f72e9d23a13abcb9f66e51a00dce365
d.zaycev <d.zaycev@bytexgames.ru>
2019-04-18 22:43:36 +0300

    Настройка github pages

--- src/environments/environments.js
+++ src/environments/environments.js
@@ -1,5 +1,5 @@
 export const environment = {
     PRODUCTION: false,
     OPWS_API_KEY: "ebf5e5843530b4f8cf4c0bd17b6b6048", //OPEN_WEATHER_MAP_API_KEY
-    OPWS_API_URL: `http://api.openweathermap.org/data/2.5`
+    OPWS_API_URL: `https://api.openweathermap.org/data/2.5`
 };
